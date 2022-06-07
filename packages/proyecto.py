123df8e1c8faee79d7ebe44f79c292ad3bafd69c
werner.uibrig <werner.uibrig@gmail.com>
2022-06-07 11:06:58 -0400

    comentarios

--- packages/proyecto.py
+++ packages/proyecto.py
@@ -22,15 +22,14 @@ class Proyecto:
         self.fechaInicio = fechaInicio
         self.fechaFin = fechaFin
         
-        self.actividades = []
+        self.actividades = []   # Lista vacia para rellenar en el proceso
         self.relaciones = []
 
-        self.final = 0
+        self.final = 0          # Contenedor de nodo final (Tipo: Actividad)
         
-        # provisional
         self.noLaborales = noLaborales
-        self.feriados = []
-        self.dias_laborales = []
+        self.feriados = []      # Para traer luego de la database
+        self.dias_laborales = []    # Cálculo
 
 
     def imprimir_proyecto(self):
@@ -42,7 +41,10 @@ class Proyecto:
         Laborales: {self.noLaborales}
             """)
 
+### Versión anterior
+
     def crear_actividad(self):
+        """Versión primitiva de crear actividad"""
         #identificador = len(self.actividades) + 1
         nombre = input("Ingrese nombre:\t")
         duracion = int(input("Ingrese duración en días:\t"))
@@ -50,7 +52,6 @@ class Proyecto:
         self.actividades.append(actividad)
 
         return actividad
-        ### Crear algún tipo de control para que ! self.actividades > 99
 
     def crear_relacion(self):
         #identificador = len(self.relaciones) + 1
@@ -61,38 +62,44 @@ class Proyecto:
 
         return relacion
 
+### 
+
+
     def actualizar_bd(self):
-        
+        """Trae los cambios desde la base de datos al programa"""
+
         self.actividades = []
-        for act in get_actividades(self.identificador):
+        for act in get_actividades(self.identificador):     # Rellena la lista de actividades de un proyecto.   
             self.actividades.append(act)
 
         self.relaciones = []
-        for rel in get_relaciones(self.identificador):
+        for rel in get_relaciones(self.identificador):      # Rellena la lista de relaciones de un proyecto desde la base de datos
             self.relaciones.append(rel)
 
         for actividad in self.actividades:
-            actividad.precedentes = [y.identificador for y in self.actividades if y.identificador in
-                [x.actividadPrecedente for x in self.relaciones if x.actividadSiguiente == actividad.identificador]]
+            actividad.precedentes = [x.actividadPrecedente for x in self.relaciones if x.actividadSiguiente == actividad.identificador]
+            # lista de actividades de las que depende cada actividad (Tipo: Identificador)
         for actividad in self.actividades: 
             actividad.siguientes = [act for act in self.actividades if actividad.identificador in act.precedentes]
+            # lista de actividades que dependen de cada actividad. (Tipo: Actividad)
         
-        self.feriados = get_feriados_date()
+        self.feriados = get_feriados_date()     # Recupera los feriados de la base de datos
 
-        for i in range(0,365):
-            dia = date.fromisoformat(self.fechaInicio) + timedelta(days = i)
-            if(date.weekday(dia) not in self.noLaborales and 
+        for i in range(0,365):                  # Para cada día desde hoy hasta un año posterior
+            dia = date.fromisoformat(self.fechaInicio) + timedelta(days = i)  # Dias del año en date
+            if(date.weekday(dia) not in self.noLaborales and   # Si ese día no es un dia no laboral o un feriado
                 sin_anho(dia) not in self.feriados):
 
-                self.dias_laborales.append(date.isoformat(dia))
+                self.dias_laborales.append(date.isoformat(dia))    # Se añaden a dias_laborales
             
 
         
     def nodo_inicio(self):
-        for actividad in self.actividades:
-            if actividad.precedentes == []:
+        """Calcular con qué nodo empieza el camino"""
+        for actividad in self.actividades:      # Para cada actividad en la lista de actividades del proyecto
+            if actividad.precedentes == []:     # Si esa actividad no tiene precedentes, es un nodo inicio.
+
                 actividad.fechaInicioTemprano = self.dias_laborales[0]
-                
                 actividad.fechaInicioTardio = self.dias_laborales[0]
                 actividad.fechaFinTardio = self.dias_laborales[self.dias_laborales.index(actividad.fechaInicioTardio) + actividad.duracion]
 
@@ -142,21 +149,6 @@ class Proyecto:
             d = [f.nombre for f in self.actividades if f.identificador == rels[a][1]]
             relsnombres.append((c[0], d[0]))
 
-        # print(rels)
-        # print(relsnombres)
-        
-        
-        # G = nx.DiGraph()
-        # G.add_edges_from(relsnombres)
-
-        # pos = nx.spring_layout(G)
-
-        # nx.draw_networkx_nodes(G,pos, node_size = 500)
-        # nx.draw_networkx_edges(G,pos, edgelist = G.edges(), edge_color= "black", arrowsize=15)
-        # nx.draw_networkx_labels(G,pos)
-
-        # mp.pyplot.show()
-
 
         g = Network(directed=True)
         nombres = [a.nombre for a in self.actividades]
@@ -172,7 +164,6 @@ class Proyecto:
             else:
                 g.add_node(nombres[i], label = identificadores[i], title = nombres[i])
         g.add_edges(relsnombres)
-        #g.show_buttons(filter_=True)
         g.show("tmp.html")
 
         
@@ -191,8 +182,8 @@ class Proyecto:
         pd.DataFrame(arr).to_csv('data.csv', index_label = "Index", header  = ['Tarea', 'Inicio', 'Fin', 'Duracion', 'Critico'])
 
 
-    def calcularFechaFin(self):
-        update_fecha_fin(self.identificador, self.fechaFin)
+    # def calcularFechaFin(self):
+    #     update_fecha_fin(self.identificador, self.fechaFin)
         
 
         
